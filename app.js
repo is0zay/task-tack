fe57032e6c4b9cffb270e1dc6df9188686e8e42a
Isaiah <isozay0@gmail.com>
2023-06-12 18:12:15 -0400

    able to write to json

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/index.html"));
--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    else{
+// document.querySelector('#push').onclick = function(){
+//     if(document.querySelector('#newtask input').value.length == 0){
+//         alert("Please Enter a Task")
+//     }
+//     else{
 		
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
+//         document.querySelector('#tasks').innerHTML += `
+//             <div class="task">
+//                 <span id="taskname">
+//                     ${document.querySelector('#newtask input').value}
+//                 </span>
+//                 <button class="delete">
+//                     <i class="far fa-trash-alt"></i>
+//                 </button>
+//             </div>
+//         `;
 
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
+//         var current_tasks = document.querySelectorAll(".delete");
+//         for(var i=0; i<current_tasks.length; i++){
+//             current_tasks[i].onclick = function(){
+//                 this.parentNode.remove();
+//             }
+//         }
 
-        var tasks = document.querySelectorAll(".task");
-        for(var i=0; i<tasks.length; i++){
-            tasks[i].onclick = function(){
-                this.classList.toggle('completed');
-            }
-        }
+//         var tasks = document.querySelectorAll(".task");
+//         for(var i=0; i<tasks.length; i++){
+//             tasks[i].onclick = function(){
+//                 this.classList.toggle('completed');
+//             }
+//         }
 
-        document.querySelector("#newtask input").value = "";
-    }
-}
+//         document.querySelector("#newtask input").value = "";
+//     }
+// }
 
-fetch('/tasks.json')
-        .then((response) => response.json())
-        .then((tasks) => {
-            var html = '';
-            for (var i = 0; i < tasks.length; i++) {
-                html += "<div class='task-tile' id='task-tile'>" +
-                    "<h2 class='task-h2'>" + tasks[i].description + "</h2>" +"</div>";
-            }
-            document.getElementById("task-list").innerHTML = html;
-        });
+// fetch('/tasks.json')
+//         .then((response) => response.json())
+//         .then((tasks) => {
+//             var html = '';
+//             for (var i = 0; i < tasks.length; i++) {
+//                 html += "<div class='task-tile' id='task-tile'>" +
+//                     "<h2 class='task-h2'>" + tasks[i].description + "</h2>" +"</div>";
+//             }
+//             document.getElementById("task-list").innerHTML = html;
+//         });
 
 
-const form = document.querySelector('form');
+// const form = document.querySelector('form');
 
-		form.addEventListener('submit', (e) => {
-			e.preventDefault();
-			const formData = new FormData(form);
-			const obj = Object.fromEntries(formData);
+// 		form.addEventListener('submit', (e) => {
+// 			e.preventDefault();
+// 			const formData = new FormData(form);
+// 			const obj = Object.fromEntries(formData);
 
-			const json = JSON.stringify(obj)
+// 			const json = JSON.stringify(obj)
 			
-		});
+// 		});
