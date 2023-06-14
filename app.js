0577a06a5e2b8f1900307216a9a8cdec86135252
Isaiah <isozay0@gmail.com>
2023-06-14 11:29:25 -0400

    Able to write to json

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.post("/", (req, res) => {
+
+	var newTaskArray = [];
+
+	newTaskArray.push(req.body);
+
+	fs.readFile('./public/tasks.json', 'utf8', (err, task) => {
+		if (err) {
+		  console.error(err);
+		  return;
+		}
+		
+		const existingData = JSON.parse(task);
+	  
+		for (let i = 0; i < newTaskArray.length; i++) {
+		  existingData.push(newTaskArray[i]);
+		}
+		console.log(existingData);
+		fs.writeFile('./public/tasks.json', JSON.stringify(existingData) , 'utf-8', (err) => {
+			if(err) {
+				return console.log(err);
+			}
+			console.log("data written");
+		})
+	});
+
+
+	res.sendFile(path.join(__dirname + "/index.html"));
+
+
+});
+
+app.listen(7000);
+
+
 	// const description = req.body.description;
 	// const complete = req.body.complete;
 	// console.log("description: " + description);
@@ -25,34 +62,32 @@ app.post("/", (req, res) => {
 	// res.sendFile('/index.html', {root: __dirname }), {flag: 'a'};
 
 	// Read the existing tasks from tasks.json file
-	fs.readFile('tasks.json', 'utf8', (err, data) => {
-		if (err) {
-		  console.error(err);
-		  return res.status(500).send('Server error');
-		}
+	// fs.readFile('tasks.json', 'utf8', (err, data) => {
+	// 	if (err) {
+	// 	  console.error(err);
+	// 	  return res.status(500).send('Server error');
+	// 	}
 	
-		let tasks = [];
-		try {
-		  tasks = JSON.parse(data);
-		} catch (err) {
-		  console.error(err);
-		  return res.status(500).send('Server error');
-		}
+	// 	let tasks = [];
+	// 	try {
+	// 	  tasks = JSON.parse(data);
+	// 	} catch (err) {
+	// 	  console.error(err);
+	// 	  return res.status(500).send('Server error');
+	// 	}
+	// 	console.log(req.body)
 	
-		// Add the new task from req.body to the tasks array
-		tasks[tasks.length] = req.body;
-	
-		// Write the updated tasks array back to the tasks.json file
-		fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf8', (err) => {
-		  if (err) {
-			console.error(err);
-			return res.status(500).send('Server error');
-		  }
-	
-		  res.status(200).sendFile('tasks added');
-		});
-	  });
-});
+	// 	// Add the new task from req.body to the tasks array
+	// 	tasks.push(req.body);
+	// 	return tasks;
+	//   });
 
-app.listen(7000);
-
+	//   // Write the updated tasks array back to the tasks.json file
+	// 	fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf8', (err) => {
+	// 		if (err) {
+	// 		  console.error(err);
+	// 		  return res.status(500).send('Server error');
+	// 		}
+	  
+	// 		res.status(200).send('tasks added');
+	// 	  });
